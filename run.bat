9b37c31e404f59124ea917b209257cffe12b05e0
prasadkolapkar123 <prasad.lonar@gmail.com>
2021-01-12 10:40:09 -0500

    This is my first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\CEPL\eclipse-workspace\inetBankingv1practiceframework
+mvn clean install
---
+++ src/main/java/inetBankingv1practiceframework/inetBankingv1practiceframework/App.java
@@ -0,0 +1,13 @@
+package inetBankingv1practiceframework.inetBankingv1practiceframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
